2e51cc79d5f39103ffca3d8507144f6f841968c4
piyushgarg01 <piyushgarg01@IN-4X3SZD3>
2024-02-20 17:20:45 +0530

    JWT Token Implementation

--- MicroServiceExample/Mango.Services.AuthAPI/Program.cs
+++ MicroServiceExample/Mango.Services.AuthAPI/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 });
 //-----------------------------------------------------------------------------------------------------------------------------------
 
-//-----------------------------------Configure Jwt properties in pipeline-------------------------------------------------------
+//-----------------------------------Configure here for Jwt token generation-------------------------------------------------------
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSettings:JwtOptions"));
 //-------------------------------------------------------------------------------------------------------------------------------
 
@@ -27,6 +27,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
