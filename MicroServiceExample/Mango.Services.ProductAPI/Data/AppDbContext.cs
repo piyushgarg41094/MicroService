2ea9351f06effad7022818b7c4975522a2270a00
piyushgarg01 <piyushgarg01@IN-4X3SZD3>
2024-02-22 16:11:42 +0530

    Perform same thing which we do for coupon API

---
+++ MicroServiceExample/Mango.Services.ProductAPI/Data/AppDbContext.cs
@@ -0,0 +1,57 @@
+﻿using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.WebRequestMethods;
+
+namespace Mango.Services.ProductAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 1,
+                Name = "Bajaj Fans",
+                Price = 2000,
+                Description = "This is an Fan manufactured by Bajaj",
+                ImageUrl = "https://placehold.co/603x403",
+                CatgoryName = "Bajaj"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 2,
+                Name = "Bajaj Cooler",
+                Price = 8000,
+                Description = "This is an Cooler manufactured by Bajaj",
+                ImageUrl = "https://placehold.co/602x402",
+                CatgoryName = "Bajaj"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 3,
+                Name = "Whirlpool washing Machine",
+                Price = 17000,
+                Description = "This is an Washing Machine manufactured by Whirlpool",
+                ImageUrl = "https://placehold.co/601x401",
+                CatgoryName = "Whirlpool"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 4,
+                Name = "Hitachi AC",
+                Price = 25000,
+                Description = "This is an AC manufactured by Hitachi",
+                ImageUrl = "https://placehold.co/600x400",
+                CatgoryName = "Hitachi"
+            });
+        }
+    }
+}
---
+++ MicroServiceExample/Mango.Services.ProductAPI/Migrations/20240222103657_addProductAndSeedTable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Mango.Services.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addProductAndSeedTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CatgoryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CatgoryName", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Bajaj", "This is an Fan manufactured by Bajaj", "https://placehold.co/603x403", "Bajaj Fans", 2000.0 },
+                    { 2, "Bajaj", "This is an Cooler manufactured by Bajaj", "https://placehold.co/602x402", "Bajaj Cooler", 8000.0 },
+                    { 3, "Whirlpool", "This is an Washing Machine manufactured by Whirlpool", "https://placehold.co/601x401", "Whirlpool washing Machine", 17000.0 },
+                    { 4, "Hitachi", "This is an AC manufactured by Hitachi", "https://placehold.co/600x400", "Hitachi AC", 25000.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
