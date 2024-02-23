734a42ced9a11c5392ba2f551bab3af662d3357a
piyushgarg01 <piyushgarg01@IN-4X3SZD3>
2024-02-23 13:32:32 +0530

    Added Models for Shopping Cart

---
+++ MicroServiceExample/Mango.Services.ShoppingCartAPI/Models/CartHeader.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Services.ShoppingCartAPI.Models
+{
+    public class CartHeader
+    {
+        [Key]
+        public int CartHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+        //By adding [NotMapped] we does not allow to create a column for discount in a table
+        [NotMapped]
+        public double Discount { get; set; }
+        [NotMapped]
+        public double CartTotal { get; set; }
+    }
+}
