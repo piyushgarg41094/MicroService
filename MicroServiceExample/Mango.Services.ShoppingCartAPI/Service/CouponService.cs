2db96f8d7f80b482e0c115d82dd22ba841625608
piyushgarg01 <piyushgarg01@IN-4X3SZD3>
2024-02-26 14:27:04 +0530

    passing bearer token from shopping cart to coupon so that we are able to access

--- MicroServiceExample/Mango.Services.ShoppingCartAPI/Service/CouponService.cs
+++ MicroServiceExample/Mango.Services.ShoppingCartAPI/Service/CouponService.cs
@@ -18,7 +18,7 @@ namespace Mango.Services.ShoppingCartAPI.Service
             var response = await client.GetAsync($"/api/coupon/GetByCode/{couponCode}");
             var apicontent = await response.Content.ReadAsStringAsync();
             var resp = JsonConvert.DeserializeObject<ResponseDto>(apicontent);
-            if(resp.IsSuccess)
+            if(resp != null && resp.IsSuccess)
             {
                 return JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(resp.Result));
             }
